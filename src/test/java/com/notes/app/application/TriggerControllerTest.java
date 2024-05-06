03a79bdde2d52b3405c9f44ccdacf49e07113d12
Chhaya Joshi (NCS) <chhaya.joshi@in.ncs-i.com>
2024-05-06 14:24:49 +0530

    test cases moved to h2 db

--- src/test/java/com/notes/app/application/TriggerControllerTest.java
+++ src/test/java/com/notes/app/application/TriggerControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class TriggerControllerTest {
 
     @Autowired
--- src/test/java/com/notes/app/infrastructure/TriggerServiceImplTest.java
+++ src/test/java/com/notes/app/infrastructure/TriggerServiceImplTest.java
@@ -9,13 +9,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.springframework.test.context.ActiveProfiles;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class TriggerServiceImplTest {
 
